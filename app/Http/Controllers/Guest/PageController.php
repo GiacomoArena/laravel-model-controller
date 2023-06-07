6af262ba2e31b2498474522b54b93e6117d2872f
GiacomoArena <giacomoarena28@gmail.com>
2023-06-07 16:10:05 +0200

    make cards

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('home');
+        $movies = Movie::all();
+
+        //dd($movie);
+
+        return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,18 @@
         <div class="container">
             <h1>Movies</h1>
 
+            <div class="container d-flex flex-wrap m-2">
+                @foreach ($movies as $movie)
+                    <div class="card m-2" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$movie->title}}</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-success">Go somewhere</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </main>
 @endsection
